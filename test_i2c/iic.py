8b595655a18b3719c86fb19b1cad1d1f0f3a0b66
hsync <felixschulze@hotmail.de>
2014-11-11 18:58:47 +0100

    update iic test script

--- test_i2c/iic.py
+++ test_i2c/iic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 
-#import smbus 
+import smbus 
 
 DEVICE_0 = 0x38                           
 DEVICE_1 = 0x39
@@ -13,38 +13,35 @@ def setLedBar(i):
 	
 	if i>8:
 		print "DEV_0: 255"
-		#bus.write_byte(DEVICE_0, 0xff)
+		bus.write_byte(DEVICE_0, 0xff)
 		i-=8
 		
 		if i>8:
 			print "DEV_1: 255"
-			#bus.write_byte(DEVICE_1, 0xff)
+			bus.write_byte(DEVICE_1, 0xff)
 			i-=8
 
 			if i>=8:
 				print "DEV_2: 255"
-				#bus.write_byte(DEVICE_2, 0xff)
+				bus.write_byte(DEVICE_2, 0xff)
 				
 			else:
 				i=(2**i)-1
 				print "DEV_2:",i
-				#bus.write_byte(DEVICE_2, i)
+				bus.write_byte(DEVICE_2, i)
 
 		else:
 	                i=(2**i)-1
 	                print "DEV_1:",i
-			#bus.write_byte(DEVICE_1 , i)
+			bus.write_byte(DEVICE_1 , i)
 
 	else:
 		i=(2**i)-1
 		print "DEV_0:",i
-		#bus.write_byte(DEVICE_0, i)
+		bus.write_byte(DEVICE_0, i)
 
 
-#	bus.write_byte(DEVICE_0,0xFF)
-
-
-#bus = smbus.SMBus(1)		# 0=/dev/i2c-0;  1=/dev/i2c-1
+bus = smbus.SMBus(1)		# 0=/dev/i2c-0;  1=/dev/i2c-1
 
 
 #Example
